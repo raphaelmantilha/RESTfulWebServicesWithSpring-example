f25f7e82b001016af694fbc2ab317d02f09417c6
Raphael Mantilha <raphael.mantilha@gmail.com>
2020-09-07 16:16:34 -0300

    Initial commit

---
+++ src/main/java/com/algaworks/socialbooks/repository/AutoresRepository.java
@@ -0,0 +1,8 @@
+package com.algaworks.socialbooks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algaworks.socialbooks.domain.Autor;
+
+public interface AutoresRepository extends JpaRepository<Autor, Long> {
+}
---
+++ src/main/java/com/algaworks/socialbooks/repository/LivrosRepository.java
@@ -0,0 +1,8 @@
+package com.algaworks.socialbooks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algaworks.socialbooks.domain.Livro;
+
+public interface LivrosRepository extends JpaRepository<Livro, Long> {
+}
